f0e53aaf35f30fef42fdbe6933be557d1e464639
MrSimbax <matt@przybyl.io>
2022-12-13 19:01:04 +0100

    improve day 13

--- day_13.lua
+++ day_13.lua
@@ -1,153 +1,65 @@
 local eio = require "libs.eio"
 local profile = require "libs.profile"
-local estring = require "libs.estring"
 
 local printf = eio.printf
-local match = string.match
-local str2tab = estring.str2tab
-local concat = table.concat
-local sort = table.sort
+local gsub = string.gsub
+local load = load or loadstring
+local type = type
 
 profile.start()
 
-local CmpRes = {
-    lt = -1,
-    eq = 0,
-    gt = 1
-}
-
-local function lt(packet1, packet2)
-    if type(packet1) ~= "table" or type(packet2) ~= "table" then
+local function lt (a, b)
+    local ta = type(a)
+    local tb = type(b)
+    if ta == "number" and tb == "number" then
+        return a < b and true or a == b and nil
+    elseif ta == "number" and tb == "table" then
+        return lt({a}, b)
+    elseif ta == "table" and tb == "number" then
+        return lt(a, {b})
+    elseif ta == "table" and tb == "table" then
+        for i = 1, #a do
+            local r = lt(a[i], b[i])
+            if r ~= nil then
+                return r
+            end
+        end
+        return #a < #b and true or nil
+    elseif tb == "nil" then
         return false
-    end
-
-    for i = 1, #packet1 do
-        local p1 = packet1[i]
-        local p2 = packet2[i]
-        if not p2 then
-            return CmpRes.gt
-        end
-        local t1 = type(p1)
-        local t2 = type(p2)
-        if t1 == "number" and t2 == "number" then
-            if p1 < p2 then
-                return CmpRes.lt
-            elseif p1 > p2 then
-                return CmpRes.gt
-            end
-        elseif t1 == "number" then
-            local r = lt({p1}, p2)
-            if r ~= CmpRes.eq then
-                return r
-            end
-        elseif t2 == "number" then
-            local r = lt(p1, {p2})
-            if r ~= CmpRes.eq then
-                return r
-            end
-        else
-            local r = lt(p1, p2)
-            if r ~= CmpRes.eq then
-                return r
-            end
-        end
-    end
-
-    if #packet1 < #packet2 then
-        return CmpRes.lt
-    elseif #packet1 == #packet2 then
-        return CmpRes.eq
-    else
-        return CmpRes.gt
     end
 end
 
+local bracesFromBrackets = {
+    ["["] = "{",
+    ["]"] = "}"
+}
+
 local function packetFromLine (line)
-    local stack = {}
-    local line = str2tab(line)
-    local i = 1
-    while i <= #line do
-        local c = line[i]
-        if c == "[" then
-            stack[#stack + 1] = {}
-            i = i + 1
-        elseif c == "]" then
-            local innerList = stack[#stack]
-            stack[#stack] = nil
-
-            local outerList = stack[#stack]
-            if outerList then
-                outerList[#outerList + 1] = innerList
-            else
-                return innerList
-            end
-
-            i = i + 1
-        elseif c == "," then
-            i = i + 1
-        else
-            local digits = {line[i]}
-            local j = i + 1
-            while match(line[j], "%d") do
-                digits[#digits + 1] = line[j]
-                j = j + 1
-            end
-            local n = tonumber(concat(digits))
-
-            local innerList = stack[#stack]
-            innerList[#innerList + 1] = n
-
-            i = j
-        end
-    end
+    return load("return " .. gsub(line, "([%[%]])", bracesFromBrackets))()
 end
 
 local input = eio.lines()
 local count = 0
 local idx = 1
-local packets = {}
+local divider1 = {{2}}
+local divider2 = {{6}}
+local pos1 = 1
+local pos2 = 2
 for i = 1, #input, 3 do
     local packet1 = packetFromLine(input[i])
     local packet2 = packetFromLine(input[i + 1])
-    if lt(packet1, packet2) == CmpRes.lt then
-        count = count + idx
-    end
-
-    packets[#packets + 1] = packet1
-    packets[#packets + 1] = packet2
+    count = count + (lt(packet1, packet2) and idx or 0)
+    pos1 = pos1 + (lt(packet1, divider1) and 1 or 0)
+    pos1 = pos1 + (lt(packet2, divider1) and 1 or 0)
+    pos2 = pos2 + (lt(packet1, divider2) and 1 or 0)
+    pos2 = pos2 + (lt(packet2, divider2) and 1 or 0)
     idx = idx + 1
 end
 
 local answer1 = count
 printf("Part 1: %i\n", answer1)
-
-packets[#packets + 1] = {{2}}
-packets[#packets + 1] = {{6}}
-local function cmp (p1, p2)
-    return lt(p1, p2) == CmpRes.lt
-end
-sort(packets, cmp)
-
-local i1 = -1
-local i2 = -1
-for i = 1, #packets do
-    local p = packets[i]
-    if #p == 1 then
-        local outer = p[1]
-        if outer and type(outer) == "table" and #outer == 1 then
-            local inner = outer[1]
-            if type(inner) == "number" then
-                if inner == 2 then
-                    i1 = i
-                elseif inner == 6 then
-                    i2 = i
-                end
-            end
-        end
-    end
-end
-
-local answer2 = i1 * i2
+local answer2 = pos1 * pos2
 printf("Part 2: %i\n", answer2)
 
 return answer1, answer2, profile.finish()
